2fb807343eff3460d5474b78beb2004425667ae9
Rickyxrc <rickyxrc@outlook.com>
2024-01-21 22:57:32 +0800

    chore: add mo file build process, add .gitignore, release v0.3.1

---
+++ scripts/build-mo.py
@@ -0,0 +1,17 @@
+import subprocess
+
+def create_mo_files(setup_kwargs):
+    print("Building mo files...")
+    for lang in ['zh_CN']:
+        subprocess.run([
+            "pybabel",
+            "compile",
+            "-i",
+            "./atcodercli/locales/%s/LC_MESSAGES/atcodercli.po" % lang,
+            "-o",
+            "./atcodercli/locales/%s/LC_MESSAGES/atcodercli.mo" % lang
+        ])
+
+if __name__ == '__main__':
+    create_mo_files({})
+
